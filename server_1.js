ad5aca2ffc86f8eb3c8820439b3b7213df983119
Galina Gatalskaya <galinagatalya@gmail.com>
2024-11-15 18:50:07 +0300

    form with redirect
    form with redirect

--- server_1.js
+++ server_1.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const webserver = express();
-webserver.use(express.urlencoded({ extended: true }));
 const port = 7381;
 
 function removeHtml(data) {
@@ -18,7 +17,7 @@ function removeHtml(data) {
 const congregateForm = (fio, errors) => {
   return `
             ${errors.name || errors.age ? 'есть ошибки, исправьте!' : ''}
-            <form action="/form" method="POST">
+            <form action="/form2">
             ${errors.name || ''}<br />
             Ваше имя (кириллица): <input type="text" name="name" 
             value="${removeHtml(fio.name)}"></input><br /><br />
@@ -28,16 +27,14 @@ const congregateForm = (fio, errors) => {
             <button type="submit">отправить</button>
             </form>`;
 };
-
 webserver.get('/form', (req, res) => {
   res.send(congregateForm({ name: '', age: '' }, {}));
 });
-
-webserver.post('/form', (req, res) => {
+webserver.get('/form2', (req, res) => {
   const errors = {};
   let flagError = false;
-  let name = req.body.name;
-  let age = req.body.age;
+  let name = req.query.name;
+  let age = req.query.age;
   if (!name || !name.match(/^[А-Яа-я]+-?[А-Яа-я]*$/)) {
     errors.name = 'Некорректное имя';
     flagError = true;
@@ -46,14 +43,11 @@ webserver.post('/form', (req, res) => {
     errors.age = 'Некорректный возраст';
     flagError = true;
   }
-  if (!flagError) res.redirect(301, `/page?name=${name}&age=${age}`);
-  else res.send(congregateForm(req.body, errors));
-});
-
-webserver.get('/page', (req, res) => {
-  let name = req.query.name;
-  let age = req.query.age;
-  res.send(`Приветствую, ${removeHtml(name)}! Ваш возраст ${removeHtml(age)}`);
+  if (!flagError)
+    res.send(
+      `Приветствую, ${removeHtml(name)}! Ваш возраст ${removeHtml(age)}`
+    );
+  else res.send(congregateForm(req.query, errors));
 });
 
 webserver.listen(port, () => {
